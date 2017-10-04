90dcfed57b6b7d7c271c586c76fda4ba87c9f811
Scott Campana <sbcampana@gmail.com>
2017-10-03 20:09:41 -0600

    added the bootstrap package, added a label and a textbox to index.php

--- include/header.php
+++ include/header.php
@@ -4,6 +4,9 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Archivo+Narrow:400,400italic,700,700italic">
 <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="../bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
 <title>aggregation.co</title>
 </head>
 <body>
+
+<div>
--- index.php
+++ index.php
@@ -4,25 +4,59 @@ require("include/header.php");
 require("include/nav.php");
 require("include/rss_util.php");
 
+// Create a function that will filter out the news sources we don't want to see based on a checkbox menu
+// The checkbox menu will run a query to pull the number of distinct news sources we have in the table
+
+// This will be a function that returns the number of distinct feed titles from the items database which will be used to generate a check box for each
+function get_sources($db){
+    $source_query = "SELECT DISTINCT feedTitle FROM items";
+    $sources = Query($db, $source_query);
+    return $sources;
+}
+
+// Create a form around this for a POST, and add the chunk of code above that will check if submit has been pressed and if it has add the new rss feed to the database and run fetshsimplepie script
+echo "<label>Add an RSS link to your feed</label>\t";
+echo "<input name='input' id='input' placeholder='RSS Link'>";
+// add a submit button that will update the database
+
+
+$sources = get_sources($db);
+echo "<div class='row'>";
+echo"<div class='col-md-6'>";
+foreach($sources as $source){
+    // check boxes for the news sources go here
+    // type=checkbox
+    // name=sourcesList[]
+    // value=$source
+    // id=$source
+    // The label is $source
+    echo "<label>";
+    //checkbox goes here
+    echo "</label>";
+}
+echo "</div>";
+echo "</div>";
+
 echo "<div id=\"content\">\n";
-echo "<div id=\"content-left\">\n";
+    echo "<div id=\"content-left\">\n";
 
-$query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=1 AND feeds.id=items.id";
-DisplayColumn($db, $query);
+        $query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=1 AND feeds.id=items.id";
+        //create a for loop that will run for each element in the list of items that need to be added to the where clause because those the the news sources we want to include.
+        DisplayColumn($db, $query);
 
-echo "</div>\n";
-echo "<div id=\"content-middle\">\n";
+    echo "</div>\n";
+    echo "<div id=\"content-middle\">\n";
 
-$query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=2 AND feeds.id=items.id";
-DisplayColumn($db, $query);
+        $query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=2 AND feeds.id=items.id";
+        DisplayColumn($db, $query);
 
-echo "</div>\n";
-echo "<div id=\"content-right\">\n";
+    echo "</div>\n";
+    echo "<div id=\"content-right\">\n";
 
-$query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=3 AND feeds.id=items.id";
-DisplayColumn($db, $query);
+        $query = "SELECT items.id AS id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc FROM feeds,items WHERE feeds.displayColumn=3 AND feeds.id=items.id";
+        DisplayColumn($db, $query);
 
-echo "</div>\n";
+    echo "</div>\n";
 echo "</div>\n";
 
 function DisplayColumn($db, $query)
