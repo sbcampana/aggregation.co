62fc298bd54343b04418714d9a1eea331d7c0b64
Scott Campana <sbcampana@gmail.com>
2017-10-03 19:28:47 -0600

    changed a few lines for an image link, but this will not be conitnued

--- fetchsimplepie.php
+++ fetchsimplepie.php
@@ -26,79 +26,118 @@ foreach ($rows as $feed) {
 
 	echo "<div>";
 	echo $content->get_title();
+	//echo "Scott!!!!!!!!! \n";
+    echo "Scott " . $content->get_image_url() . "Scott";
 	echo "</div>\n";
 
         // Display each RSS item
 	foreach ($content->get_items() as $item) {
 
-		// Check whether item already exists in the items table
-		$itemquery =
-			"SELECT * FROM items WHERE id=" .
-			$feed['id'] .
-			" AND feedLink=\"" .
-			$item->get_feed()->get_permalink() .
-			"\" AND itemLink=\"" .
-			$item->get_permalink() .
-			"\"";
+        // Check whether item already exists in the items table
+        //echo "\nThis is the item " . $item;
+        $itemquery =
+            "SELECT * FROM items WHERE id=" .
+            $feed['id'] .
+            " AND feedLink=\"" .
+            $item->get_feed()->get_permalink() .
+            "\" AND itemLink=\"" .
+            $item->get_permalink() .
+            "\"";
 
-		echo "itemquery=\"" . $itemquery . "\"\n";
+        echo "itemquery=\"" . $itemquery . "\"\n";
 
-		$itemrows = Query($db, $itemquery);
-		if (count($itemrows) == 0) {
-			echo "<div><b>";
-			echo $item->get_title();
-			echo "</b></div>";
+        $itemrows = Query($db, $itemquery);
+        if (count($itemrows) == 0) {
+            echo "<div><b>";
+            echo $item->get_title();
+            echo "</b></div>";
 
-			echo "<div>";
-			echo $item->get_local_date();
-			echo "</div>";
+            echo "<div>";
+            echo $item->get_local_date();
+            echo "</div>";
 
-			echo "<div>";
-			echo $item->get_description();
-			echo "</div>";
+            echo "<div>";
+            echo $item->get_description();
+            echo "</div>";
 
-			// Insert the item in the items table
-			if ($item->get_title() == NULL) {
+            // Insert the item in the items table
+            if ($item->get_title() == NULL) {
+                if(NULL != NULL){
 
-			$insertquery =
-				"INSERT INTO items (id,feedTitle,feedLink,itemPubDate,itemLink,itemDesc) VALUES (" .
-				$feed['id'] . ",'" . 
-				$item->get_feed()->get_title() .
-				"','" .
-				$item->get_feed()->get_permalink() .
-				"','" .
-				$item->get_local_date() .
-				"','" .
-				$item->get_permalink() .
-				"','" .
-				RemoveLinks($item->get_description()) .
-				"')";
+                $insertquery =
+                    "INSERT INTO items (id,feedTitle,feedLink,itemPubDate,itemLink,itemDesc) VALUES (" .
+                    $feed['id'] . ",'" .
+                    $item->get_feed()->get_title() .
+                    "','" .
+                    $item->get_feed()->get_permalink() .
+                    "','" .
+                    $item->get_local_date() .
+                    "','" .
+                    $item->get_permalink() .
+                    "','" .
+                    RemoveLinks($item->get_description()) .
+                    "')";
 
-			} else {
+            }else{
+                    $insertquery =
+                        "INSERT INTO items (id,feedTitle,feedLink,itemPubDate,itemLink,itemDesc, image_url) VALUES (" .
+                        $feed['id'] . ",'" .
+                        $item->get_feed()->get_title() .
+                        "','" .
+                        $item->get_feed()->get_permalink() .
+                        "','" .
+                        $item->get_local_date() .
+                        "','" .
+                        $item->get_permalink() .
+                        "','" .
+                        RemoveLinks($item->get_description()) . "', '\"Scott\"')";
+                }
+            }
+            else {
+                if(NULL != NULL) {
 
-			$insertquery =
-				"INSERT INTO items (id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc) VALUES (" .
-				$feed['id'] . ",'" . 
-				$item->get_feed()->get_title() .
-				"','" .
-				$item->get_feed()->get_permalink() .
-				"','" .
-				$item->get_title() .
-				"','" .
-				$item->get_local_date() .
-				"','" .
-				$item->get_permalink() .
-				"','" .
-				RemoveLinks($item->get_description()) .
-				"')";
+                    $insertquery =
+                        "INSERT INTO items (id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc) VALUES (" .
+                        $feed['id'] . ",'" .
+                        $item->get_feed()->get_title() .
+                        "','" .
+                        $item->get_feed()->get_permalink() .
+                        "','" .
+                        $item->get_title() .
+                        "','" .
+                        $item->get_local_date() .
+                        "','" .
+                        $item->get_permalink() .
+                        "','" .
+                        RemoveLinks($item->get_description()) .
+                        "')";
+                }
+                else {
+                    $insertquery =
+                        "INSERT INTO items (id,feedTitle,feedLink,itemTitle,itemPubDate,itemLink,itemDesc, image_url) VALUES (" .
+                        $feed['id'] . ",'" .
+                        $item->get_feed()->get_title() .
+                        "','" .
+                        $item->get_feed()->get_permalink() .
+                        "','" .
+                        $item->get_title() .
+                        "','" .
+                        $item->get_local_date() .
+                        "','" .
+                        $item->get_permalink() .
+                        "','" .
+                        RemoveLinks($item->get_description()) . "', '\"Scott\"')";
 
-			}
+                }
 
-		echo "insertquery=\"" . $insertquery . "\"\n";
+            }
 
-			Query($db, $insertquery);
-		}
-	}
+
+            echo "insertquery=\"" . $insertquery . "\"\n";
+
+            Query($db, $insertquery);
+        }
+    }
 }
 
 require("include/footer.php");
